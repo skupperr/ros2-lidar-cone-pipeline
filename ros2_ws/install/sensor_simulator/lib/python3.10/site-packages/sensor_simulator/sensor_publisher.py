8c1680eaa3130747ec8bbfc956fa6e7767964c77
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2026-04-28 18:43:28 +0200

    publisher subscriber node

---
+++ ros2_ws/install/sensor_simulator/lib/python3.10/site-packages/sensor_simulator/sensor_publisher.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+import random
+
+
+class SensorPublisher(Node):
+
+    def __init__(self):
+        super().__init__('sensor_publisher')
+
+        self.publisher = self.create_publisher(Float32, 'sensor_data', 10)
+
+        timer_period = 1.0
+        self.timer = self.create_timer(timer_period, self.publish_sensor_data)
+
+    def publish_sensor_data(self):
+        msg = Float32()
+
+        msg.data = random.uniform(20.0, 30.0)
+
+        self.publisher.publish(msg)
+
+        self.get_logger().info(f'Publishing: {msg.data:.2f}')
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    node = SensorPublisher()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
