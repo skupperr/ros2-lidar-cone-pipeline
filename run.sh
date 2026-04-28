ecb42f42564dbe153acfe4cabd2bdc9ea7e5a5f6
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2026-04-28 19:35:06 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE_NAME="ros2_sensor_simulation"
+
+echo "Building Docker image: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+echo "Running Docker container from image: $IMAGE_NAME"
+docker run --rm $IMAGE_NAME
